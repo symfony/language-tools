061512a78c8f732d9224dd01a5e51bfd1a7731fc
Fabien Potencier <fabien@potencier.org>
2026-08-05 12:30:58 +0200

    -

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,19 +1,12 @@
 <?php
 
 $finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__.'/bin')
     ->in(__DIR__.'/src')
     ->in(__DIR__.'/tests')
     ->in(__DIR__.'/tools')
     ->exclude('Fixtures/RuntimeApplication')
-    ->append([
-        __FILE__,
-        __DIR__.'/bin/symfony-lsp',
-        __DIR__.'/tools/benchmark-server',
-        __DIR__.'/tools/dogfood-server',
-        __DIR__.'/tools/dogfood-symfonycorp',
-        __DIR__.'/tools/release',
-        __DIR__.'/tools/smoke-test-server',
-    ])
+    ->append([__FILE__])
 ;
 
 return (new PhpCsFixer\Config())
