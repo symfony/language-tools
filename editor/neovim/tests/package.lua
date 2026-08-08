c50a130ff8ff4945c5b87d26a83b1bef2a03a7fa
Fabien Potencier <fabien@potencier.org>
2026-08-08 09:31:33 +0200

    Test installation through vim.pack

--- editor/neovim/tests/package.lua
+++ editor/neovim/tests/package.lua
@@ -4,6 +4,8 @@ if not vim.version.ge(vim.version(), { 0, 12, 0 }) then
   return
 end
 
+vim.opt.packpath:append(vim.fs.joinpath(vim.fn.stdpath('data'), 'site'))
+
 local source = assert(vim.env.SYMFONY_LSP_PLUGIN_SOURCE)
 local revision = assert(vim.env.SYMFONY_LSP_PLUGIN_REVISION)
 vim.pack.add({
@@ -16,7 +18,7 @@ vim.pack.add({
 
 assert(require('symfony_lsp.version'))
 assert(vim.lsp.config.symfony_lsp.cmd[1] == 'symfony-lsp')
-assert(vim.iter(vim.fn.tagfiles()):any(function(path)
+assert(vim.iter(vim.api.nvim_get_runtime_file('doc/tags', true)):any(function(path)
   return path:find('symfony%-lsp%-package%-test/doc/tags') ~= nil
 end))
 print('Neovim package test passed')
