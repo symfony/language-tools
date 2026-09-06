164556c7d51531fab02d683bc38da32951320d89
Fabien Potencier <fabien@potencier.org>
2026-09-06 07:15:42 +0200

    Use native XML reference decoding for XLIFF

--- tests/Feature/Translation/XliffXmlReferenceDecoderTest.php
+++ tests/Feature/Translation/XliffXmlReferenceDecoderTest.php
@@ -19,7 +19,8 @@ final class XliffXmlReferenceDecoderTest extends TestCase
     {
         yield 'predefined' => ['&amp;&lt;&gt;&apos;&quot;', '&<>\'"'];
         yield 'numeric' => ['&#65;&#x1F600;', "A\u{1F600}"];
-        yield 'single pass' => ['&amp;lt;', '&lt;'];
+        yield 'numeric boundaries' => ['&#9;&#10;&#13;&#32;&#xD7FF;&#xE000;&#xFFFD;&#x10000;&#x10FFFF;', "\t\n\r \u{D7FF}\u{E000}\u{FFFD}\u{10000}\u{10FFFF}"];
+        yield 'single pass' => ['&amp;lt; &amp;#X41;', '&lt; &#X41;'];
         yield 'declared and HTML' => ['&declared; &nbsp;', '&declared; &nbsp;'];
         yield 'invalid code points' => ['&#0; &#xD800; &#xFFFE; &#x110000;', '&#0; &#xD800; &#xFFFE; &#x110000;'];
         yield 'invalid syntax and overflow' => ['&#X41; &#-1; &#99999999999999999999; &amp', '&#X41; &#-1; &#99999999999999999999; &amp'];
