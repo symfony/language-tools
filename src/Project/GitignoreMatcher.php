7dbc62929085ea81dd97e10614e699b3d935f3f4
Fabien Potencier <fabien@potencier.org>
2026-09-09 12:51:51 -0700

    Rely on the upstream Finder gitignore matcher instead of a local reimplementation

--- src/Project/GitignoreMatcher.php
+++ src/Project/GitignoreMatcher.php
@@ -3,12 +3,13 @@
 namespace Symfony\Lsp\Project;
 
 use Symfony\Component\Filesystem\Path;
+use Symfony\Component\Finder\Iterator\VcsIgnoredFilterIterator;
 
 final class GitignoreMatcher
 {
     public function isIgnored(string $rootPath, string $path): bool
     {
-        return (new GitignoreEvaluation($rootPath))->isIgnored($path);
+        return 0 === iterator_count($this->filter([$path], $rootPath));
     }
 
     /**
@@ -18,12 +19,26 @@ final class GitignoreMatcher
      */
     public function filter(iterable $files, string $rootPath): \Generator
     {
-        $evaluation = new GitignoreEvaluation($rootPath);
-        foreach ($files as $file) {
-            $path = Path::canonicalize((string) $file);
-            if (!$evaluation->isIgnored($path)) {
-                yield $path;
+        $lexicalFiles = (static function () use ($files): \Generator {
+            foreach ($files as $file) {
+                $path = Path::canonicalize((string) $file);
+                yield $path => new LexicalPathFileInfo($path);
             }
+        })();
+
+        foreach (new VcsIgnoredFilterIterator($lexicalFiles, Path::canonicalize($rootPath)) as $file) {
+            yield $file->getPathname();
         }
+    }
+}
+
+/**
+ * Keeps the lexical path: symlinked roots would defeat realpath matching and deleted files have none.
+ */
+final class LexicalPathFileInfo extends \SplFileInfo
+{
+    public function getRealPath(): string
+    {
+        return $this->getPathname();
     }
 }
